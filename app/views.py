6888184b23b3158722d80980a8a22891ff1ca8bb
mihaillatyshov <35631710+mihaillatyshov@users.noreply.github.com>
2022-11-09 21:20:00 +0300

    Small fixes

--- app/views.py
+++ app/views.py
@@ -1,26 +1,32 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
+
 from . import models
 
-def paginate(objectsList, request, perPageObjects = 10):
+
+def paginate(objectsList, request, perPageObjects=10):
     paginator = Paginator(objectsList, perPageObjects)
-    return paginator.get_page(request.GET.get('page', 1)) 
+    return paginator.get_page(request.GET.get('page', 1))
 
 
-def Context(context = None):
-    res = { 'tags': models.TEST_TAGS }
-    if context != None: 
+def Context(context=None):
+    res = {'tags': models.TEST_TAGS}
+    if context != None:
         res.update(context)
     return res
 
 
 def index(request):
-    context = Context({ 'questions': models.TEST_QUESTIONS, 'page_obj': paginate(models.TEST_QUESTIONS, request) })
+    context = Context({
+        'questions': models.TEST_QUESTIONS,
+        'page_obj': paginate(models.TEST_QUESTIONS, request)})
     return render(request=request, template_name='index.html', context=context)
 
 
 def question(request, id: int):
-    context = Context({ 'question': models.TEST_QUESTIONS[id], 'answers': models.TEST_ANSWERS })
+    context = Context({
+        'question': models.TEST_QUESTIONS[id],
+        'answers': models.TEST_ANSWERS})
     return render(request, 'question.html', context=context)
 
 
@@ -29,10 +35,10 @@ def new_question(request):
 
 
 def tag(request, id: int):
-    paginator = Paginator(models.TEST_QUESTIONS, 5)
-    pageNumber = request.GET.get('page', 1)
-    page_obj = paginator.get_page(pageNumber) 
-    context = Context({ 'questions': models.TEST_QUESTIONS, 'page_obj': paginate(models.TEST_QUESTIONS, request), 'tag': models.TEST_TAGS[id] })
+    context = Context({
+        'questions': models.TEST_QUESTIONS,
+        'page_obj': paginate(models.TEST_QUESTIONS, request),
+        'tag': models.TEST_TAGS[id]})
     return render(request, 'question_by_tag.html', context=context)
 
 
@@ -45,7 +51,7 @@ def register(request):
     context = Context()
     return render(request, 'register.html', context=context)
 
-    
+
 def settings(request):
     context = Context({'profile': models.TEST_PROFILE})
     return render(request, 'settings.html', context=context)
